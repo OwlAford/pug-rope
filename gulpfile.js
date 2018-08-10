b0f00d8c291cdfa07ef3baa7bbf1c169ae274373
OwlAford <244734834@qq.com>
2018-08-10 20:35:41 +0800

    update

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+'use strict'
+const ip = require('ip')
+const opn = require('opn')
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+const chalk = require('chalk')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const connect = require('gulp-connect')
+const autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('pug', () =>
+  gulp.src('pug/pages/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./pages'))
+)
+
+gulp.task('pug:watch', () =>
+  gulp.watch('pug/**/*.pug', ['pug'])
+)
+
+gulp.task('sass', () =>
+  gulp.src('./sass/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(
+    sass({
+      outputStyle: 'expanded'
+    })
+    .on('error', sass.logError)
+  )
+  .pipe(autoprefixer({
+    browsers: [
+      '> 1%',
+      'last 2 versions'
+    ],
+    cascade: false
+  }))
+  .pipe(sourcemaps.write('./maps'))
+  .pipe(gulp.dest('./css'))
+)
+
+gulp.task('reload', () => {
+  gulp.src('./')
+    .pipe(connect.reload())
+})
+
+gulp.task('sass:watch', () =>
+  gulp.watch('./sass/**/*.scss', ['sass'])
+)
+
+gulp.task('file:watch', () => {
+  gulp.watch(['./css/*.css', './images/**/*', './pages/*.html'], ['reload'])
+})
+
+gulp.task('serve', () => {
+  const port = 8888
+  const uri = `http://${ip.address()}:${port}`
+  const lanUrlForTerminal = uri + '/pages'
+  const localUrlForTerminal = `http://localhost:${port}/pages`
+  connect.server({
+    livereload: true,
+    // root: 'static',
+    port
+  })
+  console.log(`Server is running at:\n`);
+  console.log(
+    `  ${chalk.bold('Local:')}    ${localUrlForTerminal}`
+  )
+  console.log(
+    `  ${chalk.bold('Network:')}  ${lanUrlForTerminal}\n`
+  )
+  opn(localUrlForTerminal)
+})
+
+gulp.task('start', ['sass', 'pug', 'sass:watch', 'pug:watch', 'file:watch', 'serve'])
